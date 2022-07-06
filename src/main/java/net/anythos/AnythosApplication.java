cfa012d70422c60b08e42e73a6a396b2d5f20d8a
Bartosz76 <mierzwinski.b@gmail.com>
2022-07-06 22:27:42 +0200

    The initial commit.

---
+++ src/main/java/net/anythos/AnythosApplication.java
@@ -0,0 +1,13 @@
+package net.anythos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnythosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnythosApplication.class, args);
+    }
+
+}
