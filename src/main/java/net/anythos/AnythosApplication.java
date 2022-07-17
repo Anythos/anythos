af6bc4c45f0791fe3948e1fed2b357e7945a4c7c
ABorkowska <alina-st@go2.pl>
2022-07-17 17:32:14 +0200

    Authentication entry point class + small adjustments

--- src/main/java/net/anythos/AnythosApplication.java
+++ src/main/java/net/anythos/AnythosApplication.java
@@ -3,6 +3,10 @@ package net.anythos;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+
+import java.time.ZonedDateTime;
 
 @Slf4j
 @SpringBootApplication
@@ -10,6 +14,11 @@ public class AnythosApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AnythosApplication.class, args);
+    }
+    
+    @EventListener(ApplicationReadyEvent.class)
+    public void logStartTime() {
+        log.info("#### Application started at: {} ####", ZonedDateTime.now());
     }
 
 }
