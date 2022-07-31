1fa41bc34622aa369502d7d2304c438358cb46d0
ABorkowska <alina-st@go2.pl>
2022-07-31 19:50:18 +0200

    Role entity added, user-role mapping added, data.sql adjusted

---
+++ src/main/java/net/anythos/user/entity/Role.java
@@ -0,0 +1,23 @@
+package net.anythos.user.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Table(name = "roles")
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class Role {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private String name;
+	
+}
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,8 @@
-insert into users (password, status, username) VALUES ('542634905830', 'ACTIVE', 'mgoreck')
+insert into roles
+VALUES (1,'ROLE_USER'),
+       (2,'ROLE_ADMIN'),
+       (3,'ROLE_MANAGER');
+insert into users
+VALUES (1,'54Dumbo', 'ACTIVE', 'mgoreck'),
+       (2,'Catalina27', 'ACTIVE', 'bsabala');
+insert into user_role values (1,1), (2,1), (2,3);
