fa9cb06bca7d11b7851c7bbcbfe8d99f832f08a5
Shonsu <shonsu82@gmail.com>
2022-08-26 23:44:33 +0200

    rearranged security (CrossOrigin added in controllers)

--- src/main/java/net/anythos/authorization/LoginController.java
+++ src/main/java/net/anythos/authorization/LoginController.java
@@ -3,10 +3,7 @@ package net.anythos.authorization;
 import lombok.extern.slf4j.Slf4j;
 import net.anythos.security.security_config.CredentialsDTO;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
@@ -16,6 +13,7 @@ import javax.validation.Valid;
 @RequestMapping("/anythos")
 public class LoginController {
 
+    @CrossOrigin(origins = "http://localhost:8080")
     @PostMapping("/login")
     public ResponseEntity<Void> loginUser(@RequestBody @Valid CredentialsDTO credentialsDTO,
                                           HttpServletRequest request) {
--- src/main/java/net/anythos/user/controller/UserApi.java
+++ src/main/java/net/anythos/user/controller/UserApi.java
@@ -17,6 +17,7 @@ import java.net.URI;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:8080")
 @RequestMapping("/anythos")
 public class UserApi {
 	@Autowired
